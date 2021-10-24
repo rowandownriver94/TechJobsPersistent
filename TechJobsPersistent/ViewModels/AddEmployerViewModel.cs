3e3b4d506d754a458d97e33e0bbde8071e7b4520
Rowan Polster <rowandownriver94@gmail.com>
2021-10-24 13:33:03 -0500

    Added mysql code to part 1 and created AddEmployerViewModel

---
+++ TechJobsPersistent/ViewModels/AddEmployerViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using TechJobsPersistent.Models;
+
+namespace TechJobsPersistent.ViewModels
+{
+    public class AddEmployerViewModel
+    {
+        [Required(ErrorMessage = "Please enter the name of the employer.")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Please enter a location.")]
+        public string Location { get; set; }
+        public AddEmployerViewModel(string name, string location)
+        {
+            Name = name;
+            Location = location;
+        }
+
+        public AddEmployerViewModel()
+        {
+
+        }
+    }
+}
